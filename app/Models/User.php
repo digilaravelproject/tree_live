1857189d950b57f8545a19792193d4f468e138f8
shaikhzaid01 <shaikhzaid468@gmail.com>
2026-04-18 18:12:08 +0530

    profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'phone', // This is your Mobile Number
+        'phone',
         'password',
         'role_id',
         'designation',
